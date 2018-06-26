79ca46c6e4fe101c17793fde64ec6e03ab477ab5
Devlin Verley <verle006@umn.edu>
2018-06-26 12:49:15 -0500

    Create BusinessList component

---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './BusinessList.css';
+import '../components/Business.js'
+
+class BusinessList extends React.Component {
+    render() {
+        <div className="BusinessList">
+            <Business />
+            <Business />
+            <Business />
+            <Business />
+            <Business />
+            <Business />
+        </div>
+    }
+};
